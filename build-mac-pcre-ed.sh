8911a5e1ed02ba8af4e90323ee272873f0c211a3
jonbakke <49371446+jonbakke@users.noreply.github.com>
2020-09-07 10:33:31 -0700

    Fix shell escaping.

--- build-mac-pcre-ed.sh
+++ build-mac-pcre-ed.sh
@@ -13,6 +13,6 @@ curl -O  $url || wget $url || fetch_failed
 wait
 tar xzf ed-1.16.tar.lz
 cd ed-1.16
-printf %s\n "/CFLAGS/s=O2=O2 -I/usr/local/include" "/LDFLAGS/s=\$=\-L/usr/local/lib -lpcre2-posix -lpcre2-8" w q | ed configure
-printf %s\n /regex.h/s//pcre2posix.h/ w q | ed regex.c
+printf '%s\n' '/CFLAGS/s=O2=O2 -I/usr/local/include' "/LDFLAGS/s=$='-L/usr/local/lib -lpcre2-posix -lpcre2-8'" w q | ed configure
+printf '%s\n' /regex.h/s//pcre2posix.h/ w q | ed regex.c
 ./configure && make && cp ed ../pcre-ed && echo "Success!"
